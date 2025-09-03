ff87b6ac2c5f995ecd1c1091ae96ef0a5180eb5f
Bruno <bruno.taveira.r0.382@gmail.com>
2025-09-03 15:01:23 -0300

    feat: controlar a quantidade cards que são renderizados implementado

--- frontend/src/components/NewsCardList/NewsCardList.jsx
+++ frontend/src/components/NewsCardList/NewsCardList.jsx
@@ -1,16 +1,27 @@
 import "./NewsCardList.css"
+import { useState } from 'react'
 import NewsCard from "../NewsCard/NewsCard.jsx"
 
+const cardsData = [1, 2, 3, 4, 5, 6]
+
 function NewsCardList(){
+	const [showAll, setShowAll] = useState(false)
+	const visibleCards = showAll ? cardsData : cardsData.slice(0,3)
 	return(
 		<>
 			<div className="cardlist">
 				<h2 className="cardlist__title">Procurar resultados</h2>
 				<div className="cardlist__wrapper">
-					<NewsCard />
-					<NewsCard />
-					<NewsCard />
+					{visibleCards.map((card,i) => (
+						<NewsCard key={i} />
+					))}
 				</div>
+				{cardsData.length > 3 && (
+					<button className="cardlist__btn-showAll" onClick={() => setShowAll(!showAll)}>
+						{showAll ? "Mostrar menos" : "Mostrar mais"}
+					</button>
+				)}
+
 			</div>
 		</>
 	)
