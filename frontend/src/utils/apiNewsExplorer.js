c68997d0f4d105c5da769810b3133c54546d828c
Bruno <bruno.taveira.r0.382@gmail.com>
2025-09-10 13:29:01 -0300

    .

--- frontend/src/utils/apiNewsExplorer.js
+++ frontend/src/utils/apiNewsExplorer.js
@@ -2,7 +2,7 @@ class Api {
 
 	constructor({ newsApiKey}) {
 		this._newsApiKey = newsApiKey
-		this._baseUrl = "https://news-explorer-xwja.onrender.com/"
+		this._baseUrl = "https://news-explorer-xwja.onrender.com"
 	}
 
 	async _makeRequest(url, method, body, token) {
