6e1a1eed57a4f2e34ed33bd0d947771de782e004
Bruno <bruno.taveira.r0.382@gmail.com>
2025-09-10 12:21:51 -0300

    feat: menu mobile implementado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,13 +2,18 @@ import './Header.css'
 import { usePopup } from "../App/App.jsx"
 import { useAuth } from "../../contexts/AuthContext.jsx"
 import logoutImg from "../../images/logout.png"
+import { useState } from 'react'
 
-function Header({}) {
-
-	const {openPopup} = usePopup()
+function Header() {
+	const { openPopup } = usePopup()
 	const { user, logout } = useAuth()
+	const [menuMobileOpen, setMenuMobileOpen] = useState(false)
 
-	return(
+	const toggleMenuMobile = () => {
+		setMenuMobileOpen((open) => !open)
+	}
+
+	return (
 		<>
 			<div className="header">
 				<div className="header__navigation">
@@ -22,13 +27,37 @@ function Header({}) {
 							<button className="header__navigation-btn-signin" onClick={logout}>
 								{user.name}
 								<img src={logoutImg} alt="Sair" />
-								</button>
+							</button>
 						) : (
 							<button className="header__navigation-btn-signin" onClick={openPopup}>Entrar</button>
 						)}
 					</div>
+					{/* Botão hambúrguer */}
+					<div className={`header__menu-mobile-icon${menuMobileOpen ? " active" : ""}`} onClick={toggleMenuMobile}>
+						<div className="line"></div>
+						<div className="line"></div>
+					</div>
 				</div>
 			</div>
+			{/* Menu mobile */}
+			{menuMobileOpen && (
+				<div className="header__menu-mobile-panel">
+					<a className="header__navigation__mobile-logo" href="/">NewsExplorer</a>
+					<div className="header__navigation-wrapper">
+						<a className="header__navigation-btn-home" href="/" onClick={toggleMenuMobile}>Início</a>
+						{user && (
+							<a className="header__navigation-btn-home" href="/saved-news" onClick={toggleMenuMobile}>Artigos salvos</a>
+						)}
+						{user ? (
+							<button className="header__navigation-btn-signin" onClick={() => { logout(); toggleMenuMobile(); }}>
+								Sair
+							</button>
+						) : (
+							<button className="header__navigation-btn-signin" onClick={() => { openPopup(); toggleMenuMobile(); }}>Entrar</button>
+						)}
+					</div>
+				</div>
+			)}
 		</>
 	)
 }
