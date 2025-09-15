212185ff7acc6f0dbbb6efde2161638e6e09c159
rochtbruno <bruno.taveira.r0.382@gmail.com>
2025-09-15 14:04:24 -0300

    feat: loading para login e cadastro implementado

--- frontend/src/components/PopupWithForm/PopupWithForm.jsx
+++ frontend/src/components/PopupWithForm/PopupWithForm.jsx
@@ -12,6 +12,7 @@ function PopupWithForm({ onClose }){
 	const [password, setPassword] = useState("")
 	const [name, setName] = useState("")
 	const [error, setError] = useState("")
+	const [loading, setLoading] = useState(false)
 	const { login } = useAuth()
 
 	useEffect(() => {
@@ -31,6 +32,7 @@ function PopupWithForm({ onClose }){
 	async function handleSubmit(e){
 		e.preventDefault()
 		setError("")
+		setLoading(true)
 		try {
 			if(type === "login"){
 				const {token} = await api.signinUser({ email, password })
@@ -46,6 +48,7 @@ function PopupWithForm({ onClose }){
 		} catch (error) {
 			setError(error.message || "Erro ao autenticar usuário")
 		}
+		setLoading(false)
 	}
 
 	return(
@@ -54,58 +57,68 @@ function PopupWithForm({ onClose }){
 				<div className="popupwithform" ref={popupRef}>
 					<img className="popupwithform__close" src={closePopup} alt="fechar popup" onClick={onClose}/>
 					<h2 className="popupwithform__title">{type === "login" ? "Entrar" : "Inscreva-se"}</h2>
-					<form className="popupwithform__form" onSubmit={handleSubmit}>
-						<label>
-							Email <br />
-							<input 
-							className="popupwithform__input" 
-							type="email" 
-							placeholder="Insira seu email" 
-							required 
-							value={email}
-							onChange={e => setEmail(e.target.value)}
-							/>
-						</label>
-						{type === "register" && (
+					{loading ? (
+						<div className="popupwithform__loader">
+                            <div className="loader-spinner"></div>
+                            <p>Aguarde...</p>
+                        </div>
+				) : (
+					<>
+						<form className="popupwithform__form" onSubmit={handleSubmit}>
 							<label>
-								Nome de usuário <br />
+								Email <br />
 								<input 
 								className="popupwithform__input" 
-								type="text" 
-								placeholder="Insira seu nome de usuário" 
-								required
-								value={name}
-								onChange={e => setName(e.target.value)}
+								type="email" 
+								placeholder="Insira seu email" 
+								required 
+								value={email}
+								onChange={e => setEmail(e.target.value)}
 								/>
 							</label>
-						)}
-						<label >
-							Senha <br />
-							<input 
-							className="popupwithform__input" 
-							type="password" 
-							placeholder="Insira sua senha" 
-							required
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-							/>
-						</label>
-						<button type="submit" className="popupwithform__submit">
-							{type === "login" ? "Entrar" : "Inscrever-se"}
-						</button>
-					</form>
-					<a 
-					className="popupwithform__link" 
-					href="/"
-					onClick={(e) => {
-						e.preventDefault()
-						if(type === "login"){
-							setType("register")
-						}else{
-							setType("login")
-						}
-					}}
-					>{type === "login" ? "Inscrever-se" : "Entrar"}</a>
+							{type === "register" && (
+								<label>
+									Nome de usuário <br />
+									<input 
+									className="popupwithform__input" 
+									type="text" 
+									placeholder="Insira seu nome de usuário" 
+									required
+									value={name}
+									onChange={e => setName(e.target.value)}
+									/>
+								</label>
+							)}
+							<label >
+								Senha <br />
+								<input 
+								className="popupwithform__input" 
+								type="password" 
+								placeholder="Insira sua senha" 
+								required
+								value={password}
+								onChange={e => setPassword(e.target.value)}
+								/>
+							</label>
+							<button type="submit" className="popupwithform__submit">
+								{type === "login" ? "Entrar" : "Inscrever-se"}
+							</button>
+						</form>
+						<a 
+							className="popupwithform__link" 
+							href="/"
+							onClick={(e) => {
+								e.preventDefault()
+								if(type === "login"){
+									setType("register")
+								}else{
+									setType("login")
+								}
+							}}
+							>{type === "login" ? "Inscrever-se" : "Entrar"}</a>
+					</>
+				)}
+					
 				</div>
 			</div>
 		</>
